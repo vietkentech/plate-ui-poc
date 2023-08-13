9058ae440975432d278758092ead1878b86f1058
The Anh Nguyen <ntheanh201@gmail.com>
2023-08-13 23:57:41 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import BasicEditor from './BasicEditor'
+import { Button } from './components/plate-ui/button'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click me</Button>
+        <BasicEditor />
+      </div>
+    </>
+  )
+}
+
+export default App
